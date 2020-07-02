9de7436fa5dc82a696cee8f44f1b382843a4cc57
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-02 15:53:06 -0500

    sets up local host

--- server/server.js
+++ server/server.js
@@ -6,3 +6,11 @@ const bodyParser = require('body-parser');
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.post('/math', (req, res) => {
+  res.sendStatus(201);
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on localhost:', PORT);
+});
