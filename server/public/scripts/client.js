f6e29f16fd508aacfb579dff545a63799321449c
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-02 16:31:24 -0500

    sets up functions and click events

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,30 @@
 $(document).ready(init);
 
 function init() {
-  console.log('Ready');
+  $('#jsaddbtn').on('click', addEm);
+  $('#jssubbtn').on('click', subIt);
+}
+
+function addEm() {
+  console.log('Gonna Add Here');
+}
+
+function subIt() {
+  console.log('Gonna Subtract Here');
+}
+
+function multiply() {
+  console.log('Gonna Multiply Here');
+}
+
+function divide() {
+  console.log('Gonna Divide Here');
+}
+
+function doDaMath() {
+  console.log('I probably need a function for the = btn');
+}
+
+function clearIt() {
+  console.log('Gonna Clear It Here');
 }
